6d008a21a6504c348039cb665d9b0807b68c2c7e
Abiud Sokomi <abiudsokomi@gmail.com>
2024-01-15 06:24:01 +0300

    my commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require("node:fs");
+
+const readableStream = fs.createReadStream("./file.txt", {
+    encoding: "utf-8",
+    highWaterMark: 2,
+})
+const writeableStream = fs.createWriteStream("./file2.txt");
+
+readableStream.on("data", (chunk)=>{
+    console.log(chunk);
+    writeableStream.write(chunk);
+})
+
+// optionally: readableStream.pipe(writeableStream);
